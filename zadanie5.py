332fe9e68cc4d166ba7937ad73a03e70184669e5
pkumanowski <pkumanowski@outlook.com>
2021-11-14 15:55:59 +0100

    new py file added

---
+++ zadanie5.py
@@ -0,0 +1,25 @@
+cenaPizza1 = float(input("podaj cene 1 pizzy: "))
+srednica1 = float(input("podaj srednice 1 pizzy: "))
+cenaPizza2 = float(input("podaj cene 2 pizzy: "))
+srednica2 = float(input("podaj srednice 2 pizzy: "))
+
+pi = 3.14
+
+
+
+promien1 = srednica1 / 2
+promien2 = srednica2 / 2
+pole1 = pi * promien1 * promien1
+pole2 = pi * promien2 * promien2
+
+oplacalnosc1 = pole1 / cenaPizza1
+oplacalnosc2 = pole2 / cenaPizza2
+print("oplacalnosce pierwszej to: ", oplacalnosc1)
+print("oplacalnosce drugiej to: ", oplacalnosc2)
+
+if oplacalnosc1 > oplacalnosc2:
+    print("pierwsza pizza jest bardziej opłacalna")
+elif oplacalnosc1 == oplacalnosc2:
+    print("pizze sa tak samo oplacalne")
+else:
+    print("druga pizza jest bardziej opłacalna")
